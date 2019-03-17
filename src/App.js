cd59259bfdad063456460efc3b86bd96b43572b9
Emanuel Rosa <fenix.gusmao+github@gmail.com>
2019-03-17 16:39:41 -0300

    feat: add personal page. close #2

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Hello World</h1>
+
+        <h1>emanuelRosa</h1>
+        <h2>신비한 사람</h2>
+
+        <nav>
+          <ul>
+          <li>
+            <a href="https://github.com/#">Github</a>
+          </li>
+          <li>
+            <a href="https://twitter.com/#">Twitter</a>
+          </li>
+          </ul>
+        </nav>
+
       </div>
     );
   }
