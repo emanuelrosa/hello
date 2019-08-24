6bc8b5eceb07924fa683df7c27b014d46a174ee5
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-08-24 15:29:03 +0000

    Deploy emanuelrosa/hello to github.com/emanuelrosa/hello.git:gh-pages

---
+++ precache-manifest.4f49537752067fe1f8c0ce10c6b78dd8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eceded0efb7e26133eed600d3be4620e",
+    "url": "/hello/index.html"
+  },
+  {
+    "revision": "8c735bfe4c82827aff5e",
+    "url": "/hello/static/css/main.1f28e1b8.chunk.css"
+  },
+  {
+    "revision": "fe37e9024530b46c3a24",
+    "url": "/hello/static/js/2.918a718d.chunk.js"
+  },
+  {
+    "revision": "8c735bfe4c82827aff5e",
+    "url": "/hello/static/js/main.4a1f82bf.chunk.js"
+  },
+  {
+    "revision": "e809f0e8826a3ecfa9de",
+    "url": "/hello/static/js/runtime~main.6c698878.js"
+  }
+]);
